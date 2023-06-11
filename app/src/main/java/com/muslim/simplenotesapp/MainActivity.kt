afb8fcf79f8417d904081114334ea09c584d823b
Murad <engno_95@mail.ru>
2023-06-11 12:45:50 +0300

    fixed navigation. The UI of the application has been changed.

--- app/src/main/java/com/muslim/simplenotesapp/MainActivity.kt
+++ app/src/main/java/com/muslim/simplenotesapp/MainActivity.kt
@@ -4,32 +4,19 @@ import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.muslim.simplenotesapp.navigation.NavRoute
 import com.muslim.simplenotesapp.navigation.NotesNavHost
 import com.muslim.simplenotesapp.ui.theme.SimpleNotesAppTheme
-import com.muslim.simplenotesapp.utils.DB_TYPE
 
 class MainActivity : ComponentActivity() {
 
@@ -42,38 +29,6 @@ class MainActivity : ComponentActivity() {
                     viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
                 val navController = rememberNavController()
                 Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(horizontal = 16.dp),
-                                    horizontalArrangement = Arrangement.SpaceBetween
-                                ) {
-                                    Text(text = "Notes App")
-                                    if (DB_TYPE.value.isNotEmpty()) {
-                                        Icon(
-                                            imageVector = Icons.Default.ExitToApp,
-                                            contentDescription = null,
-                                            modifier = Modifier
-                                                .clickable {
-                                                    viewModel.signOut {
-                                                        navController.navigate(NavRoute.Start.route) {
-                                                            popUpTo(NavRoute.Start.route) {
-                                                                inclusive = true
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                        )
-                                    }
-                                }
-                            },
-                            backgroundColor = Color.Blue,
-                            contentColor = Color.White
-                        )
-                    },
                     content = { paddingValues ->
                         Surface(
                             modifier = Modifier
